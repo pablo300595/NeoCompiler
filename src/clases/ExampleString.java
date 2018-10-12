c723c114481903232efb85866b878e91eb1b7e79
pablo300595 <jopavalenzuelami@ittepic.edu.mx>
2018-10-11 19:48:20 -0500

    feat: Inclusión de archivo con Strings de ejemplo
    Solo se añade clase auxiliar para pruebas de ejecución

---
+++ src/clases/ExampleString.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author pablo
+ */
+public class ExampleString {
+    public static String example1;
+    public ExampleString(){
+        example1="task _hola params none{\n"
+            + "Objeto a==6$\n"
+            + "&\n"
+            + "¡\n"
+            + "}";
+    }
+}
